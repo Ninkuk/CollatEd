941576446fe867b959d2b6c99ce51d99f821da3b
ninkuk <ninadk03@gmail.com>
2019-09-01 19:48:04 -0700

    finished the challenges form with validation

--- public/js/form.js
+++ public/js/form.js
@@ -1,19 +1,28 @@
 const challengeForm = document.querySelector("#challenge-form");
+document.getElementById("thanks-message").style.display = "none";
 
 var discipline = "";
 var goal = "";
 
-function selectDiscipline(discipline){
+function selectDiscipline(discipline) {
     this.discipline = discipline;
+    document.getElementById("discipline-button").innerText = discipline;
 }
 
-function selectGoal(goal){
+function selectGoal(goal) {
     this.goal = goal;
+    document.getElementById("goal-button").innerText = goal;
 }
 
 challengeForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('cafes').add({
+
+    if (challengeForm.name.value == "" || challengeForm.institutionName.value == "" || challengeForm.challengeTitle.value == "" || this.discipline == "" || this.goal == "" || challengeForm.problemDescription.value == "" || challengeForm.guidelinesDescription.value == "" || challengeForm.resources.value == "") {
+        alert("Please enter the required fields!");
+        return false;
+    }
+
+    db.collection('challenges').add({
         name: challengeForm.name.value,
         institutionName: challengeForm.institutionName.value,
         challengeTitle: challengeForm.challengeTitle.value,
@@ -25,4 +34,6 @@ challengeForm.addEventListener('submit', (e) => {
         resources: challengeForm.resources.value
     });
     challengeForm.reset();
+    document.getElementById("challenge-form").style.display = "none";
+    document.getElementById("thanks-message").style.display = "block";
 })
